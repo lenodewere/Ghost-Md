94d7ad12fdf18e42414573fde05bf0e020835781
Gifted Devs <166598671+mouricedevs@users.noreply.github.com>
2024-04-29 20:16:44 +0300

    𝑮𝒉𝒐𝒔𝒕-𝑴𝒅

--- commands/Ping.js
+++ commands/Ping.js
@@ -38,7 +38,7 @@ france({ nomCom: 'ping',
   async (dest, zk, commandeOptions) => {
     const { ms, arg, repondre } = commandeOptions;
     const { start} = new Date().getTime()
-    return repondre('*Pong!!*\n ```' + 2000 + '``` *ms*') 
+    return repondre('*𝑮𝒉𝒐𝒔𝒕-𝑴𝒅 𝑺𝒑𝒆𝒆𝒅:*\n ```' + 𝟏𝟐𝟎 + '``` *ms*') 
     const { end } = new Date().getTime()
     await zok.sendMessage('*Pong!*\n ```' + (end - start) + '``` *ms*')
   }
